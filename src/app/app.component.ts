0339ca2c84b67400f1de756f7083706e20853063
Phil Ellis <philip.ellis.j@gmail.com>
2019-05-31 16:26:59 -0400

    fixed bug in navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,32 @@ import { Component } from '@angular/core';
 })
 
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  selectedItem
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.forEach((event) => {
+      if(event instanceof NavigationEnd) {
+        if(this.router.url == '/') {
+          this.selectedItem = this.links[0].name;
+        } else {
+          this.links.forEach((value) => {
+            if(value.url === this.router.url) {
+              this.selectedItem = value.name;
+            }
+          })
+        }
+      }
+    });
+
+   }
   links = [
     {name: 'Home', url: '/home'},
     {name: 'Create Survey', url: '/create'},
     {name: 'Survey Results', url: '/results'}
   ]
-  selectedItem = this.links[0].name;
+  
   listClick(event, newValue) {
     this.selectedItem = newValue.name;
 }
